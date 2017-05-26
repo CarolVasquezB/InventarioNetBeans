e9e0b4939a30960f7754896ecf5033b5cb8e0633
1061785255 <1061785255@S109_PC08.fup.edu>
2017-05-25 19:26:41 -0500

    Ventana Principal

--- Inventario_Tirsa/src/Vista/GUIPrincipal.java
+++ Inventario_Tirsa/src/Vista/GUIPrincipal.java
@@ -27,17 +27,79 @@ public class GUIPrincipal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jMenu3 = new javax.swing.JMenu();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenu2 = new javax.swing.JMenu();
+        jMenu4 = new javax.swing.JMenu();
+        jMenu5 = new javax.swing.JMenu();
+        jMenu6 = new javax.swing.JMenu();
+        jMenu7 = new javax.swing.JMenu();
+
+        jMenu3.setText("jMenu3");
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMaximumSize(new java.awt.Dimension(650, 440));
+        setMinimumSize(new java.awt.Dimension(650, 440));
+        setPreferredSize(new java.awt.Dimension(650, 440));
+
+        jLabel2.setFont(new java.awt.Font("Tempus Sans ITC", 1, 14)); // NOI18N
+        jLabel2.setText("Usuario:");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(25, 25, 25)
+                .addComponent(jLabel2)
+                .addContainerGap(567, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(23, 23, 23)
+                .addComponent(jLabel2)
+                .addContainerGap(377, Short.MAX_VALUE))
+        );
+
+        jMenu1.setText("Facturación ");
+        jMenu1.setFont(new java.awt.Font("Segoe UI", 3, 12)); // NOI18N
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Clientes");
+        jMenu2.setFont(new java.awt.Font("Segoe UI", 3, 12)); // NOI18N
+        jMenuBar1.add(jMenu2);
+
+        jMenu4.setText("Inventario");
+        jMenu4.setFont(new java.awt.Font("Segoe UI", 3, 12)); // NOI18N
+        jMenuBar1.add(jMenu4);
+
+        jMenu5.setText("Reportes");
+        jMenu5.setFont(new java.awt.Font("Segoe UI", 3, 12)); // NOI18N
+        jMenuBar1.add(jMenu5);
+
+        jMenu6.setText("Configuración");
+        jMenu6.setFont(new java.awt.Font("Segoe UI", 3, 12)); // NOI18N
+        jMenuBar1.add(jMenu6);
+
+        jMenu7.setText("Salir");
+        jMenu7.setFont(new java.awt.Font("Segoe UI", 3, 12)); // NOI18N
+        jMenuBar1.add(jMenu7);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -79,5 +141,15 @@ public class GUIPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenu jMenu3;
+    private javax.swing.JMenu jMenu4;
+    private javax.swing.JMenu jMenu5;
+    private javax.swing.JMenu jMenu6;
+    private javax.swing.JMenu jMenu7;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
