ad399e3748f912b9f04e9b1c9a351684fc5b311f
1061792939 <1061792939@S109_PC09.fup.edu>
2017-05-25 19:06:27 -0500

    control empleado Rol

--- Inventario_Tirsa/src/Control/ControlEmpleado_Rol.java
+++ Inventario_Tirsa/src/Control/ControlEmpleado_Rol.java
@@ -11,6 +11,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -31,28 +32,28 @@ public boolean insertarEmpleado_Rol(int cod_empleado_rol,int cod_empleado_person
         return inserto;        
     } 
     
-    public boolean eliminarRol(int cod_rol){
+    public boolean eliminarEmpleado_Rol(int cod_empleado_rol){
         boolean elimino = false;
-        String sql = "Delete from rol where cod_rol = " + cod_rol;
+        String sql = "Delete from empleado_rol where cod_empleado_rol = " + cod_empleado_rol;
         elimino = p.ejecutarDML(sql);
         return elimino;
     }    
     
      
     
-    public boolean actualizarRol(int cod_rol,String nombre_rol){
+    public boolean actualizarEmpleado_Rol(int cod_empleado_rol,int cod_empleado_persona,int cod_rol){
         boolean actualizo = false;
         
-        String sql = "Update rol set "
-                + "nombre_rol= '"+nombre_rol+"' where cod_rol= "+cod_rol;
+        String sql = "Update empleado_rol set "
+                + "cod_empleado_persona= "+cod_empleado_persona+",cod_rol="+cod_rol+" where cod_empleado_rol= "+cod_empleado_rol;
         actualizo = p.ejecutarDML(sql);
         return actualizo;
     }   
 
-    public int contarRoles(){
+    public int contarEmpleado_Roles(){
         
         int numero = 0;
-        String sql = "Select count(cod_rol) num from rol";
+        String sql = "Select count(cod_empleado_rol) num from empleado_rol";
         ResultSet res = p.ejecutarConsulta(sql);
         
         try {
@@ -68,7 +69,7 @@ public boolean insertarEmpleado_Rol(int cod_empleado_rol,int cod_empleado_person
     
     public Object[][] consultarRol(int codigo){
 
-        Object data[][] = new Object[this.contarRoles()][2];
+        Object data[][] = new Object[this.contarEmpleado_Roles()][3];
         ResultSet datos = null;
         String sql = "Select cod_rol,nombre_rol from rol "
                 + "where cod_rol = "+codigo;
@@ -92,65 +93,68 @@ public boolean insertarEmpleado_Rol(int cod_empleado_rol,int cod_empleado_person
     
     public Object[][] consultarRol(){
 
-        Object data[][] = new Object[this.contarRoles()][2];
+        Object data[][] = new Object[this.contarEmpleado_Roles()][3];
         ResultSet datos = null;
-        String sql = "Select * from rol";
+        String sql = "Select * from empleado_rol";
         datos = p.ejecutarConsulta(sql);
 
         try {
             int i = 0;
             while(datos.next()){
-                data[i][0] = datos.getInt("cod_rol");
-                data[i][1] = datos.getString("nombre_rol");
+                data[i][0] = datos.getInt("cod_empleado_rol");
+                data[i][1] = datos.getInt("cod_empleado_persona");
+                data[i][2] = datos.getInt("cod_rol");
                 
                 i++;
             }
         } catch (SQLException ex) {
-            Logger.getLogger(ControlRoles.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(ControlEmpleado_Rol.class.getName()).log(Level.SEVERE, null, ex);
         }
         return data;
     }
     
-    public static void main(String[] args) {
-        ControlEmpleado_Rol er=new ControlEmpleado_Rol();
-
-        boolean inserto=er.insertarEmpleado_Rol(1,123,1234);
-        if (inserto) {
-            System.out.println("inserto...");
-        }
-        else{
-            System.out.println("error...");
-    }
-    }
-    
-//        boolean elimino=cr.eliminarRol(12334);
-//        if (elimino) {
-//            System.out.println("eliminado");
-//        }else{
-//            System.out.println("error...");
-//        }
-    
-//        
-//        boolean actualizo=cr.actualizarRol(12334,"vendedor");
-//        if(actualizo){
-//            System.out.println("actualizo...");
-//        }else{
-//            System.out.println("error...");
-//        }
-    
-        
-        
-//        int aux=cr.contarRoles();
-//        System.out.println("el numero de roles es : "+aux);
+//    public static void main(String[] args) {
+//        ControlEmpleado_Rol er=new ControlEmpleado_Rol();
+//
+////        boolean inserto=er.insertarEmpleado_Rol(145,1,1);
+////        if (inserto) {
+////            System.out.println("inserto...");
+////            //JOptionPane.showMessageDialog(null,"se inserto correctamente!","Confirmacion",JOptionPane.INFORMATION_MESSAGE);
+////            
+////        }
+////        else{
+////            System.out.println("error...");
+////    }
+//    
+//    
+////        boolean elimino=er.eliminarEmpleado_Rol(145);
+////        if (elimino) {
+////            System.out.println("eliminado");
+////        }else{
+////            System.out.println("error...");
+////        }
+//    
+////        
+////        boolean actualizo=er.actualizarEmpleado_Rol(145,2,23);
+////        if(actualizo){
+////            System.out.println("actualizo...");
+////        }else{
+////            System.out.println("error...");
+////        }
+//   
 //        
 //        
- 
-       
-        //Prueba
-//        Object[][] dato = cr.consultarRol();        
-//        for (int i = 0; i < cr.contarRoles(); i++) {
-//                System.out.println("Codigo Rol: "+ dato[i][0]+ " nombre rol: "+dato[i][1]);
-//        }
+////        int aux=er.contarEmpleado_Roles();
+////        System.out.println("el numero de empleados_roles es : "+aux);
+////         }
+//    
+// 
+//       
+//        //Prueba
+////        Object[][] dato = er.consultarRol();
+////        for (int i = 0; i < er.contarEmpleado_Roles(); i++) {
+////                System.out.println("Codigo empleado_rol: "+ dato[i][0]+ " cod empleado persona: "+dato[i][1]+" rol "+dato[i][2]);
+////        }
 //  }
     //}
 }
