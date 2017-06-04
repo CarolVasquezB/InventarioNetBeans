a388d3699e4c2bd2f161b1b44a9e4593045069d2
David <David@DAVIDJP>
2017-06-04 13:40:14 -0500

    GUIREPORTE

--- Inventario_Tirsa/src/Control/ControlFactura.java
+++ Inventario_Tirsa/src/Control/ControlFactura.java
@@ -15,80 +15,77 @@ import java.util.logging.Logger;
  *
  * @author CarolVasquez
  */
-
 //prueba 
 //editado por david 
 public class ControlFactura {
-    
+
     Persistencia p = new Persistencia();
-    
+
     public boolean insertarFactura(int codigoFactura, float valorFactura, String fechaFactura, int codigoCliente,
-        int codigoEmpleado, int codigoAbono){
-        
+            int codigoEmpleado) {
+
         boolean inserto = false;
-        String sql = "Insert into factura (cod_factura, valor_factura, fecha_factura, cod_cliente, cod_empleado, cod_abono) "
-                + "values ("+codigoFactura+", "+valorFactura+", '"+fechaFactura+"', "+codigoCliente+", "+codigoEmpleado+","+codigoAbono+")";
+        String sql = "Insert into factura (cod_factura, valor_factura, fecha_factura, cod_cliente, cod_empleado) "
+                + "values (" + codigoFactura + ", " + valorFactura + ", '" + fechaFactura + "', " + codigoCliente + ", " + codigoEmpleado + ")";
         inserto = p.ejecutarDML(sql);
-        return inserto;        
-    } 
-    
+        return inserto;
+    }
+
     public boolean actualizarFactura(int codigoFactura, float valorFactura, String fechaFactura, int codigoCliente,
-        int codigoEmpleado, int codigoAbono){
+            int codigoEmpleado) {
         boolean actualizo = false;
         String sql = "Update factura set "
-                + "valor_factura = " +valorFactura 
-                + ", fecha_factura = '"+valorFactura  
-                + ", cod_cliente = "+codigoCliente   
-                + ", cod_empleado = "+codigoCliente       
-                + ", cod_abono = "+codigoAbono                   
-                + " where cod_factura = "+codigoFactura;
+                + "valor_factura = " + valorFactura
+                + ", fecha_factura = '" + valorFactura
+                + ", cod_cliente = " + codigoCliente
+                + ", cod_empleado = " + codigoCliente
+                + " where cod_factura = " + codigoFactura;
         actualizo = p.ejecutarDML(sql);
         return actualizo;
-    }   
+    }
 
-    public int contarFacturas(){
-        
+    public int contarFacturas() {
+
         int numero = 0;
         String sql = "Select count(cod_factura) num from factura";
         ResultSet res = p.ejecutarConsulta(sql);
-        
+
         try {
-          
-            while(res.next()){
+
+            while (res.next()) {
                 numero = res.getInt(1);
             }
         } catch (SQLException ex) {
             Logger.getLogger(ControlCategorias.class.getName()).log(Level.SEVERE, null, ex);
-        }       
+        }
         return numero;
     }
-    
-    public Object[][] consultarFactura(int codigo){
+
+    public Object[][] consultarFactura(int codigo) {
 
         Object data[][] = new Object[this.contarFacturas()][5];
         ResultSet datos = null;
-        String sql = "Select cod_factura, valor_factura, fecha_factura, cod_cliente, cod_empleado, cod_abono"
-                + " from factura where cod_factura = "+codigo;
+        String sql = "Select cod_factura, valor_factura, fecha_factura, cod_cliente, cod_empleado"
+                + " from factura where cod_factura = " + codigo;
         datos = p.ejecutarConsulta(sql);
 
         try {
             int i = 0;
-            while(datos.next()){
+            while (datos.next()) {
                 data[i][0] = datos.getInt("cod_factura");
                 data[i][1] = datos.getFloat("valor_factura");
                 data[i][2] = datos.getString("fecha_factura");
                 data[i][3] = datos.getInt("cod_cliente");
                 data[i][4] = datos.getInt("cod_empleado");
-                data[i][5] = datos.getInt("cod_abono");
                 i++;
             }
         } catch (SQLException ex) {
             Logger.getLogger(ControlCategorias.class.getName()).log(Level.SEVERE, null, ex);
         }
         return data;
-    }     
-         
-    public Object[][] consultarFacturas(){
+    }
+
+    public Object[][] consultarFacturas() {
 
         Object data[][] = new Object[this.contarFacturas()][5];
         ResultSet datos = null;
@@ -97,18 +94,37 @@ public class ControlFactura {
 
         try {
             int i = 0;
-            while(datos.next()){
+            while (datos.next()) {
                 data[i][0] = datos.getInt("cod_factura");
                 data[i][1] = datos.getFloat("valor_factura");
                 data[i][2] = datos.getString("fecha_factura");
                 data[i][3] = datos.getInt("cod_cliente");
-                data[i][4] = datos.getInt("cod_empleado");
-                data[i][5] = datos.getInt("cod_abono");
+                data[i][4] = datos.getInt("cod_empleado");              
                 i++;
             }
         } catch (SQLException ex) {
             Logger.getLogger(ControlCategorias.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return data;    
+        return data;
     }
+
+    public Object[][] consultarCodMaxFactura() {
+        ResultSet datos = null;
+        Object data[][] = new Object[1][1];
+        String sql = "select max(cod_factura) num from factura";
+        int num=0;
+        datos = p.ejecutarConsulta(sql);
+        try {
+            while (datos.next()) {
+                data[0][0] = datos.getString("num");    
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ControlCategorias.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return data;
+    }
+
+//    public static void main(String[] args) {
+//
+//    }
 }
--- Inventario_Tirsa/src/Control/ControlReporte.java
+++ Inventario_Tirsa/src/Control/ControlReporte.java
@@ -9,6 +9,7 @@ import Modelo.ConexionBD;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Date;
 import java.util.logging.Level;
@@ -21,34 +22,43 @@ import java.util.logging.Logger;
 public class ControlReporte {
 
     Connection con =null;
-
+    ResultSet rs=null;
     public Object[][] Generar_reporte(String fecha_inicial, String fecha_final) {
         Object [][]data=null;
         try {
             
             CallableStatement cst=null;
             con = DriverManager.getConnection("jdbc:mysql://localhost/inventario", "root","mysql");
-           cst = con.prepareCall("{call Reporte_Fechas (?,?,?,?,?,?,?,?)}");
+           cst = con.prepareCall("{call Reporte_Fechas (?,?,?)}");
             
-             data = new Object[0][5];
+             data = new Object[5][5];
 
             cst.setString(1,fecha_inicial);
             cst.setString(2,fecha_final);
             cst.registerOutParameter(3, java.sql.Types.INTEGER);
-            cst.registerOutParameter(4, java.sql.Types.FLOAT);
-            cst.registerOutParameter(5, java.sql.Types.DATE);
-            cst.registerOutParameter(6, java.sql.Types.INTEGER);
-            cst.registerOutParameter(7, java.sql.Types.INTEGER);
-            cst.registerOutParameter(8, java.sql.Types.INTEGER);
+//            cst.registerOutParameter(4, java.sql.Types.FLOAT);
+//            cst.registerOutParameter(5, java.sql.Types.DATE);
+//            cst.registerOutParameter(6, java.sql.Types.INTEGER);
+//            cst.registerOutParameter(7, java.sql.Types.INTEGER);
+//            cst.registerOutParameter(8, java.sql.Types.INTEGER);
+            
+            rs=cst.executeQuery();
+           
+             try {
             int i = 0;
-            while (i != cst.getInt(8)) {
-               data[i][0]=cst.getInt(3);
-               data[i][1]=cst.getFloat(4);
-               data[i][2]=cst.getDate(5);
-               data[i][3]=cst.getInt(6);
-               data[i][4]=cst.getInt(7);
+            while (rs.next()) {
+                data[i][0] = rs.getInt("cod_factura");
+                data[i][1] = rs.getFloat("valor_factura");
+                data[i][2] = rs.getDate("fecha_factura");
+                data[i][3] = rs.getInt("cod_cliente");
+                data[i][4] = rs.getInt("cod_empleado");
                 i++;
             }
+        } catch (SQLException ex) {
+            Logger.getLogger(ControlReporte.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            
+            
 
         } catch (SQLException ex) {
             Logger.getLogger(ControlReporte.class.getName()).log(Level.SEVERE, null, ex);
